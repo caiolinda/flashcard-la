4a7decbb9dee97f4ed7e8e6e95c67b7480c7b0ea
caiolinda <146266623+caiolinda@users.noreply.github.com>
2024-10-24 12:17:05 -0300

    Add files via upload

--- flashcards/app.js
+++ flashcards/app.js
@@ -3,4 +3,15 @@ function criaCartao (categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+    cartao.innerHTML = `
+    <!-- <article class="cartao">
+    <div class="cartao__conteudo"> 
+        <h3>${CATEGORIA}</h3>
+        <div class="cartao__conteudo__pergunta">
+            <P>${PERGUNTA}</P></div>
+        <div class="cartao__conteudo__resposta">
+            <P>${RESPOSTA}</P></div>
+    </div> -->
+    `
+    container.appendChild(cartao)
 }
