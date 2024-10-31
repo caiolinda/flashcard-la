3fc985ed09121ba6aa8906bf7866e03e7369fab5
caiolinda <146266623+caiolinda@users.noreply.github.com>
2024-10-31 12:15:57 -0300

    Add files via upload

--- flashcards/app.js
+++ flashcards/app.js
@@ -2,16 +2,23 @@ function criaCartao (categoria, pergunta, resposta) {
     console.log(categoria, pergunta, resposta)
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
-    cartao.className = 'cartao'
+    cartao.className = "cartao"
     cartao.innerHTML = `
     <!-- <article class="cartao">
     <div class="cartao__conteudo"> 
-        <h3>${CATEGORIA}</h3>
+        <h3>${categoria}</h3>
         <div class="cartao__conteudo__pergunta">
-            <P>${PERGUNTA}</P></div>
+            <p>${pergunta}</p></div>
         <div class="cartao__conteudo__resposta">
-            <P>${RESPOSTA}</P></div>
-    </div> -->
+            <p>${resposta}</p></div>
+    </div> 
+    </article> -->
     `
+    let respostaEstaVisivel = false
+    function viraCartao(){
+        respostaestaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+        cartao.addEventListener('click', viraCartao)
+    }
     container.appendChild(cartao)
 }
